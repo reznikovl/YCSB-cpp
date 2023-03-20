1ce8b19b3e6c53848959e457d3a1c652c757760c
EC2 Default User <ec2-user@ip-172-30-1-124.us-west-2.compute.internal>
2023-03-20 22:33:13 +0000

    caching for tests

--- fig1.py
+++ fig1.py
@@ -5,15 +5,14 @@ import subprocess
 import csv
 
 # mb to write
-mb_to_write = [1024*1, 1024*2, 1024*4, 1024*6, 1024*8, 1024*10, 1024 * 12, 1024 * 14, 1024 * 18, 1024 * 22, 1024 * 25]
-# mb_to_write = [1024*1, 1024*2, 1024*4, 1024*6]
+mb_to_write = [1024*1, 1024*2, 1024*4, 1024*6, 1024*8, 1024*10]
 db_path = "/home/ec2-user/research/mountpt/"
 
 # T (ratio for leveling)
 T = 4
 
 # k (ratio for Autumn)
-k = 4
+k = 3
 
 # autumn parameter to use
 c = 0.8
@@ -36,10 +35,23 @@ f.write(f"key size (bytes) = {key_size_bytes}\n")
 f.write(f"value size (bytes) = {value_size_bytes}\n")
 
 # add list to track results
-result_fields = ["Operation", "Size (MB)", "Ratio", "Time"]
+result_fields = ["Operation", "Size (MB)", "Ratio", "Latency"]
 results = []
 
-
+def clear_cache():
+    f2 = None
+    try:
+        f2 = open("/proc/sys/vm/drop_caches", "a")
+        r = subprocess.run(["sync"])
+        r2 = subprocess.run(["echo", "3"], stdout=f2)
+        if r.returncode != 0 or r2.returncode != 0:
+            print("Error clearing cache...")
+        else:
+            print("Cleared Cache")
+    except Exception as e:
+        print("Exception when opening /proc/ file for caching:", e)
+        print("Try running with sudo or double checking machine reqs")
+    
 
 if (sys.argv[1] == "1"):
     # do writes
@@ -56,16 +68,16 @@ if (sys.argv[1] == "1"):
         curr_command += ["-p", f"operationcount={op_count}"]
         curr_command += ["-p", f"leveldb.dbname={db_path}fig1_base_{num_mb}"] # must be last
 
-        r = subprocess.run(curr_command, capture_output=True, encoding="utf-8")
-        f.write("\n-----BASE WRITE " + str(num_mb) + " -----\n")
         f.write(str(curr_command))
 
-        parse_location = r.stdout.find("Run throughput")
-        num = float((r.stdout[parse_location + 24:]).strip())  # magic number :(
-        results.append(("WRITE", num_mb, 1, num))
-
+        r = subprocess.run(curr_command, capture_output=True, encoding="utf-8")
+        f.write("\n-----BASE WRITE " + str(num_mb) + " -----\n")
         f.write(r.stderr)
         f.write(r.stdout)
+        
+        parse_location = r.stdout.rfind("Avg=")
+        num = float(r.stdout[parse_location:].split()[0].split("=")[1])
+        results.append(("WRITE", num_mb, 1, num))
         
         f.write("\n-----BASE WRITE FINISHED-----\n")
 
@@ -79,8 +91,8 @@ if (sys.argv[1] == "1"):
         f.write("\n-----TEST WRITE " + str(num_mb) + "-----\n")
         f.write(str(curr_command))
 
-        parse_location = r2.stdout.find("Run throughput")
-        num2 = float((r2.stdout[parse_location + 24:]).strip())  # magic number :(
+        parse_location = r.stdout.rfind("Avg=")
+        num2 = float(r.stdout[parse_location:].split()[0].split("=")[1])
         results.append(("WRITE", num_mb, c, num2))
         
         f.write(r2.stderr)
@@ -110,6 +122,8 @@ if int(sys.argv[1]) >= 1:
     
     os.chdir("../../YCSB-cpp")
 
+clear_cache()
+
 # Perform Point Reads
 base_write_args = ["./ycsb", "-run", "-db",
                    "leveldb", "-P", "workloads/read_uniform", "-s", "-p", 'leveldb.filter_bits=5,5,5,5,5,5,5']
@@ -123,14 +137,14 @@ for num_mb in mb_to_write:
     r = subprocess.run(curr_command, capture_output=True, encoding="utf-8")
     f.write("\n-----BASE READ " + str(num_mb) + " -----\n")
     f.write(str(curr_command))
-
-    parse_location = r.stdout.find("Run throughput")
-    num = float((r.stdout[parse_location + 24:]
-                 ).strip())  # magic number :(
-    results.append(("READ", num_mb, 1, num))
-
     f.write(r.stderr)
     f.write(r.stdout)
+
+    parse_location = r.stdout.rfind("Avg=")
+    num = float(r.stdout[parse_location:].split()[0].split("=")[1])
+    results.append(("READ", num_mb, 1, num))
+
+    
 
     f.write("\n-----BASE READ FINISHED-----\n")
 
@@ -138,21 +152,26 @@ for num_mb in mb_to_write:
     curr_command += [f"leveldb.dbname={db_path}fig1_test_{num_mb}"]
     curr_command += ["-p", f"ratio_diff={c}"]
     print(f"Reading from db with {num_mb} mb test...")
-    r2 = subprocess.run(
-        curr_command, capture_output=True, encoding="utf-8")
+
     f.write("\n-----TEST READ " + str(num_mb) + "-----\n")
     f.write(str(curr_command))
 
-    parse_location = r2.stdout.find("Run throughput")
-    num2 = float((r2.stdout[parse_location + 24:]
-                  ).strip())  # magic number :(
-    results.append(("READ", num_mb, c, num2))
+    r2 = subprocess.run(
+        curr_command, capture_output=True, encoding="utf-8")
     
     f.write(r2.stderr)
     f.write(r2.stdout)
+
+    parse_location = r2.stdout.rfind("Avg=")
+    num2 = float(r2.stdout[parse_location:].split()[0].split("=")[1])
+    results.append(("READ", num_mb, c, num2))
+    
+    
     f.write("\n-----TEST READ FINISHED-----\n")
 
 print(results)
+
+clear_cache()
 
 
 # Perform Range Reads
@@ -171,14 +190,15 @@ for num_mb in mb_to_write:
     f.write(str(curr_command))
     r = subprocess.run(curr_command, capture_output=True, encoding="utf-8")
 
-    parse_location = r.stdout.find("Run throughput")
-    num = float((r.stdout[parse_location + 24:]
-                 ).strip())  # magic number :(
+    f.write(r.stderr)
+    f.write(r.stdout)
+
+    parse_location = r.stdout.rfind("Avg=")
+    num = float(r.stdout[parse_location:].split()[0].split("=")[1])
     results.append(("SCAN", num_mb, 1, num))
 
     
-    f.write(r.stderr)
-    f.write(r.stdout)
+    
 
     f.write("\n-----BASE SCAN FINISHED-----\n")
 
@@ -190,13 +210,13 @@ for num_mb in mb_to_write:
     f.write(str(curr_command))
     r2 = subprocess.run(
         curr_command, capture_output=True, encoding="utf-8")
-
-    parse_location = r2.stdout.find("Run throughput")
-    num2 = float((r2.stdout[parse_location + 24:]
-                  ).strip())  # magic number :(
-    results.append(("SCAN", num_mb, c, num2))
     f.write(r2.stderr)
     f.write(r2.stdout)
+
+    parse_location = r2.stdout.rfind("Avg=")
+    num2 = float(r2.stdout[parse_location:].split()[0].split("=")[1])
+    results.append(("SCAN", num_mb, c, num2))
+    
 
     f.write("\n-----TEST SCAN FINISHED-----\n")
 
