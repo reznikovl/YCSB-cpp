83ac628ff6923a38b9eb4300d8f626a2ebfed311
Lron Reznikov <reznikovl@gmail.com>
2023-04-26 19:20:03 +0000

    don't count first n operations

--- core/db_factory.cc
+++ core/db_factory.cc
@@ -30,7 +30,7 @@ DB *DBFactory::CreateDB(utils::Properties *props, Measurements *measurements) {
   if (registry.find(db_name) != registry.end()) {
     DB *new_db = (*registry[db_name])();
     new_db->SetProps(props);
-    db = new DBWrapper(new_db, measurements);
+    db = new DBWrapper(new_db, measurements, props);
   }
   return db;
 }
